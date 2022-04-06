fe358819d94e2037fef3e5377e872c4fca24ecea
H. Lockyer <hl785@bath.ac.uk>
2022-04-06 11:57:24 +0100

    Rotation/Exp test

--- demoScripts.jl
+++ demoScripts.jl
@@ -1,6 +1,20 @@
 include("main.jl")
 
 #----------------------------------------------------------------------------------------------------
+# If the register is in the state Rot(PauliX, theta) * |0>, then expectation wrt PauliZ is cos(theta)
+theta = 1.0                                             # Test parameter
+trueRes = cos(theta)                                    # Targer result
+
+register = Ket([ComplexF64(1,0), ComplexF64(0,0)])      # Set up in initial condition |0>
+opr = rotGate(paX2(), theta)                            # Create a gate
+register = opr * register                               # Apply a gate to the register
+testRes = oprExpectation(paZ2(), register)              # Measure / expectation
+
+println("\nTest rotation gates and expectations:\n")    # Print
+println("True result: ", trueRes)
+println("Quan result: ", testRes)
+println("Error: ", abs(trueRes - testRes))
+#----------------------------------------------------------------------------------------------------
 
 # a = ComplexF64(1,2)
 # b = ComplexF64(4,6)
