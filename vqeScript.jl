5265d13fe2db633ac443c92c33fa105aa1a096a4
H. Lockyer <hl785@bath.ac.uk>
2022-05-17 13:43:29 +0100

    Tidy

--- vqeScript.jl
+++ vqeScript.jl
@@ -5,46 +5,46 @@ using LinearAlgebra
 include("main.jl")
 
 numQBits = 1
+initReg = rBs0() ^ numQBits
+x0 = [1.0, 0.0]
+mtd = "Nelder-Mead"
+
 dimension = 2^numQBits
 H=randP(ComplexF64, dimension)
 hOpr = Opr(H)
-# heatmap(broadcast(abs, hOpr.vals), yflip = true)
 
-register = rBs0() ^ numQBits
-# heatmap(broadcast(abs, kto(register).vals), yflip = true)
-
-# U = randU(ComplexF64, dimension)
-# uOpr = Opr(U)
-# heatmap(broadcast(abs, uOpr.vals), yflip = true)
-
-function uParam(a::Float64, b::Float64)::Opr
-    return out = matmul(rotGate(paZ2(), b), rotGate(paX2(), a))
+function uParam(a::Array{Float64, 1})::Opr
+    if numQBits == 1
+        @assert length(x0) == 2 "Wrong number of initial parameters"
+        return matmul(rotGate(paZ2(), a[2]), rotGate(paX2(), a[1]))
+    else
+        @assert false "Not coded this number of QBits"
+    end
+    return scalarArray(ComplexF64(1,0))
 end
 
-function cost(a::Float64, b::Float64, print::Bool)::Float64
-    regCopy = register
-    uOpr = uParam(a,b)
-    regCopy = uOpr * regCopy
-    eVal = oprExpectation(hOpr, regCopy)
+function cost(a::Array{Float64, 1}, print::Bool)::Float64
+    register = initReg
+    uOpr = uParam(a)
+    register = uOpr * register
+    eVal = oprExpectation(hOpr, register)
     if print
         # println("Estimated eigenval:  ", eVal)
         println("Estimated eigenval:  ", eVal.re)
-        println("Estimated eigenvec:  ", regCopy.vals[1], ", ", regCopy.vals[2])
-        println("Eigenpair residual:  ", norm((hOpr * regCopy).vals - (eVal.re * regCopy.vals)))
+        println("Estimated eigenvec:  ", register.vals[1], ", ", register.vals[2])  # TODO: longer evals print
+        println("Eigenpair residual:  ", norm((hOpr * register).vals - (eVal.re * register.vals)))
     end
     return eVal.re
 end 
 
-function costVecIn(a::Array{Float64, 1})::Float64
-    return cost(a[1], a[2], false)
+function costNoOpt(a::Array{Float64, 1})::Float64
+    return cost(a, false)
 end
 
-x0 = [1.0, 0.0]
-
-res = SciPy.optimize.minimize(costVecIn, x0, method="Nelder-Mead", tol=1e-6)
+res = SciPy.optimize.minimize(costNoOpt, x0, method = mtd, tol=1e-6)
 
 # Check
-eValRe = cost(res["x"][1], res["x"][2], true)
+eValRe = cost(res["x"], true)
 trueEVal = eigvals(H, 1:1)[1]
 println("Calculated eigenval: ", trueEVal)
 println("Eigenval error:      ", abs(trueEVal - eValRe))
